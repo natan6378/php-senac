ec037614f13bc276b34496a4bc8c0e1f2d469c76
natan pereira <natanp637@gmail.com>
2024-03-20 22:14:34 -0300

    variaveis, contatenação e interpolação

---
+++ 01/teste.php
@@ -0,0 +1,41 @@
+<?php
+
+echo "Olá Mundo!<br>";
+
+//issso é um comentario
+
+# isso tambem é um comentario de multiplas linhas
+
+/*
+isso é um comentario de multiplas linhas
+eu, disse, multiplas linhas
+*/
+
+//declaração de variaveis
+//$nome ="Natan <br>";
+//$sobrenome= "Pereira <br>";
+$nome_completo = $nome . " ".  $sobrenome;
+
+//ECHO $nome ; 
+//echo $sobrenome;
+
+echo $nome . " ".  $sobrenome;
+echo $nome_completo
+
+//variaveis numericas
+$idade = 25;
+$ano = 2024;
+
+echo $idade . "<br>";
+echo $ano - $idade; //operações matematicas
+
+echo 'Esse é um texto em aspas simples <br>';
+echo "esse é um texto em aspas duplas <br>";
+
+//trabalhando com aspas simples e duplas 
+echo 'Don Pedro disse:"independencia ou morte"';
+
+echo "$nome tem $idade anos de idade <br>";
+'echo "$nome tem $idade anos de idade <br>";'
+
+?>
